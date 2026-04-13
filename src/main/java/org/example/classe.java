894f002aa7a2e44e2080ef4b2dd86d76dc64ce51
Nauannems <nauannesantosmarinho@email.com>
2026-04-13 09:34:38 -0300

    sei la

---
+++ src/main/java/org/example/classe.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class classe {
+    String aulagit;
+
+    public classe(String aulagit) {
+        this.aulagit = aulagit;
+    }
+
+    public String getAulagit() {
+        return aulagit;
+    }
+
+    public void setAulagit(String aulagit) {
+        this.aulagit = aulagit;
+    }
+}
