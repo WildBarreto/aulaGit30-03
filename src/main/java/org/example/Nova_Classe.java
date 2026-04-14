6ffd620708d779b3232fd0f9168fe17543ddd494
Allana Vitória <@allana.vitoriaamorim013@gmail.com>
2026-04-14 20:45:49 -0300

    Nova Classe

---
+++ src/main/java/org/example/Nova_Classe.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Nova_Classe {
+    String classe;
+
+    public Nova_Classe(String classe) {
+        this.classe = classe;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+}
